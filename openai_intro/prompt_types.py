ee40a4c8cecd32bd6c838b52dc2a84f0d48d8486
Vijaya K Eyunni <evkumar71@yahoo.com>
2025-06-07 13:11:38 -0400

    chain-of-thought prompt ex

--- openai_intro/prompt_types.py
+++ openai_intro/prompt_types.py
@@ -7,31 +7,39 @@ my_model = 'gpt-4o-mini'
 load_dotenv()
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-# client = OpenAI()
 
 # ==few-shot learning or prompting
-completion = client.chat.completions.create(
-                model=my_model,
-                messages=[
-                    {'role':'system', 'content': 'You are a translator'},
-                    {'role' :'user',
-                     'content': """Translate these sentences: 
-                                    'Hello' -> 'Hola', 
-                                    'Goodbye' -> 'Adiós'. 
-                                    '.
-                                    Now translate: 'Thank you'."""
-                    }
-                    ]
-                )
+# completion = client.chat.completions.create(
+#                 model=my_model,
+#                 messages=[
+#                     {'role':'system', 'content': 'You are a translator'},
+#                     {'role' :'user',
+#                      'content': """Translate these sentences: 
+#                                     'Hello' -> 'Hola', 
+#                                     'Goodbye' -> 'Adiós'. 
+#                                     '.
+#                                     Now translate: 'Thank you'."""
+#                     }
+#                     ]
+#                 )
 
-# print(completion.choices[0].message.content)
-# == direct prompting
+# # print(completion.choices[0].message.content)
+# # == direct prompting
+# completion = client.chat.completions.create(
+#                 model=my_model,
+#                 messages= [
+#                     {'role': 'system', 'content': 'you are an assistang'},
+#                     {'role': 'user', 'content':"Write about climate change in 5 lines"}
+#                 ]
+#             )
+
+# == chain-of-thought prompt
 completion = client.chat.completions.create(
                 model=my_model,
                 messages= [
-                    {'role': 'system', 'content': 'you are an assistang'},
-                    {'role': 'user', 'content':"Write about climate change in 5 lines"}
+                    {'role': 'system', 'content': 'You are a match tutor'},
+                    {'role': 'user', 'content':"Solve this math problem step by step: \
+                        If John has 5 apples and gives 2 to Mary, how many does he have left?"}
                 ]
             )
-
 print(completion.choices[0].message.content)
