c1db4763b776c54cb3d33e3e11bbb865d90e135b
Vijaya K Eyunni <evkumar71@yahoo.com>
2025-06-06 22:22:57 -0400

    minor changes to prompt

--- openai_intro/openai_simple.py
+++ openai_intro/openai_simple.py
@@ -6,9 +6,10 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-response = client.chat.completions.create(model="o4-mini",
-                                          messages=[{'role': 'system', 'content': 'You are a helpful agent'},
-                                                    {'role': 'user', 'content' : 'write a few great points about llm - deepseek'}]
+response = client.chat.completions.create(
+                                    model="o4-mini",
+                                    messages=[{'role': 'system', 'content': 'You are a helpful agent'},
+                                            {'role': 'user', 'content' : 'Write a haiku poem on Google. Also, give a title to the poem'}]
                                           )
 
 print(response.choices[0].message.content)
