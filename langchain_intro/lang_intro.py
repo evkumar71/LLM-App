ae4ba56da99d3246f0a045288d407ed8319ba9a6
Vijaya K Eyunni <evkumar71@yahoo.com>
2025-06-18 21:54:37 -0400

    added intro script for langchain framework

---
+++ langchain_intro/lang_intro.py
@@ -0,0 +1,27 @@
+import getpass
+import os
+
+# if not os.environ.get("OPENAI_API_KEY"):
+#   os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+response = model.invoke("hello world ! How are you ?")
+print(response.content)
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system_template = "Translate the following from English into {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "sanskrit", "text": "How are you?"})
+
+print(prompt)
+
+response = model.invoke(prompt)
+print(response.content)
